ad1a00b6a279b0df25335e3e802f7e2217609238
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-10-06 09:31:44 +0200

    dont use db call

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -1,11 +1,11 @@
 // src/pages/api/examples.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../server/db/client";
+// import { prisma } from "../../server/db/client";
 import { env } from "../../env/server.mjs";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
-  res.status(200).json({ nextAuthUrl: env.NEXTAUTH_URL, examples });
+  // const examples = await prisma.example.findMany();
+  res.status(200).json({ nextAuthUrl: env.NEXTAUTH_URL });
 };
 
 export default examples;
