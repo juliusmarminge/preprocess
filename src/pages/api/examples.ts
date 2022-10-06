3c2a5539e6d05402bf5f8d4b90286530f333d225
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-10-06 09:29:50 +0200

    expose it

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -1,10 +1,11 @@
 // src/pages/api/examples.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
+import { env } from "../../env/server.mjs";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
   const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
+  res.status(200).json({ nextAuthUrl: env.NEXTAUTH_URL, examples });
 };
 
 export default examples;
